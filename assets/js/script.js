245cd3e10a14f0d3e4993a910d1a8301e003712b
Jarell-Chinn <JarellChinn44@gmail.com>
2024-06-03 14:35:53 -0700

    added a fade-effect class to make code more reusable

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("DOMContentLoaded", function() {
     let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-            // Toggle 'visible' class based on whether the section is intersecting
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
             } else {
                 entry.target.classList.remove('visible');
             }
         });
-    }, { threshold: 0.1 }); // The threshold can be adjusted to trigger the effect sooner or later
+    }, { threshold: 0.2 }); // The threshold can be adjusted to trigger the effect sooner or later
 
-    // Observe both work image wrappers and container-works sections
-    document.querySelectorAll('.work-image-wrapper, .container-works').forEach(element => {
+    document.querySelectorAll('.fade-effect').forEach(element => {
         observer.observe(element);
     });
 });
